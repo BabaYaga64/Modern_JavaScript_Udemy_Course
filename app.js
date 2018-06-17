50cf1631f74220d3cb3402a3379620276e506edb
Bojana Skarich <bojana.skarich@gmail.com>
2018-06-16 18:10:57 -0700

    Add console.log statements

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 alert('Hello World');
 
+
+
+/* Console.log will print out the value of the string, variable, Boolean value, or whatever is between the parentheses at page load time into the Chrome Developer console.  */
+
+console.log('Value of x is 5.');
+var greeting = 'Purple';
+console.log(greeting);
+
+console.error('This is an error');
+
+/* 3 ways to declare variables in JavaScript: 
+var, let, const
+*/
