8a6010fb81979e1459a86b4996708034b4d7c2ac
Bojana Skarich <bojana.skarich@gmail.com>
2018-06-16 18:53:17 -0700

    Add const variables

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-alert('Hello World');
-
-
+//alert('Hello World');
 
 /* Console.log will print out the value of the string, variable, Boolean value, or whatever is between the parentheses at page load time into the Chrome Developer console.  */
 
@@ -10,6 +8,29 @@ console.log(greeting);
 
 console.error('This is an error');
 
+//Initialize a variable. This sets it to an undefined value. 
+var greeting;
+
+/* Allowed characters in variables in JavaScript:
+letters, numbers, underscores, dollar signs $. Also cannot start with a number. */
+
 /* 3 ways to declare variables in JavaScript: 
 var, let, const
 */
+let name = "Esmeralda";
+console.log(name);
+
+/* Const stands for "constant". Variable cannot be reassigned. Also, you have to assign a value. */
+const cat = "Sophie";
+
+//JavaScript object with values
+const person = {
+  firstName: 'Brittany',
+  age: 30,
+  hometown: 'Portland'
+}
+/*You can change property value of object, but not the object itself (aka person) with const variable type. Cannot be reassigned as a new primitive value. */
+person.firstName = 'Jody';
+
+console.log(person);
+
